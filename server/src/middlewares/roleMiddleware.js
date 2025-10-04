d773dbf3570d0a9cd4175f8f093708406fa2840e
ImToad06 <jreyesaguilar123@gmail.com>
2025-10-03 21:22:00 -0500

    fixed spelling

--- server/src/middlewares/roleMiddleware.js
+++ server/src/middlewares/roleMiddleware.js
@@ -2,7 +2,7 @@ export const authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
       return res.status(403).json({
-        message: "Acces denied: you don't have permission to acces this route",
+        message: "Access denied: you don't have permission to acces this route",
       });
     }
     next();
