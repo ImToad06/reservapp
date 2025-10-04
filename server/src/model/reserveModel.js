152373fac5b346a5ba7ae907249745cbe501faea
ImToad06 <jreyesaguilar123@gmail.com>
2025-10-03 22:59:25 -0500

    create model for reserves

---
+++ server/src/model/reserveModel.js
@@ -0,0 +1,56 @@
+import pool from "../config/db.js";
+
+export const getAllReservesService = async () => {
+  const query = "SELECT * FROM reserves r WHERE r.status = 'a';";
+  const res = await pool.query(query);
+  return res.rows;
+};
+
+export const getReserveByIdService = async (reserveId) => {
+  const query = `
+    SELECT * FROM reserves r
+    WHERE r.status = 'a' AND r.reserve_id = $1;
+  `;
+  const res = await pool.query(query, [reserveId]);
+  return res.rows[0];
+};
+
+export const getReserveByUserService = async (UserId) => {
+  const query = `
+    SELECT * FROM reserves r
+    WHERE r.status = 'a' AND r.user = $1;
+  `;
+  const res = await pool.query(query, [UserId]);
+  return res.rows;
+};
+
+export const createReserveService = async (userId, tableId, date) => {
+  const query = `
+    INSERT INTO reserves (user, table, date, status) VALUES ($1, $2, $3, 'a')
+    RETURNING *;
+  `;
+  const res = await pool.query(query, [userId, tableId, date]);
+  return res.rows[0];
+};
+
+export const updateReserveService = async (
+  reserveId,
+  userId,
+  tableId,
+  date,
+) => {
+  const query = `
+    UPDATE reserves SET user = $1, table = $2, date = $3
+    WHERE reserve_id = $4 RETURNING *;
+  `;
+  const res = await pool.query(query, [userId, tableId, date, reserveId]);
+  return res.rows[0];
+};
+
+export const deleteReserveService = async (reserveId) => {
+  const query = `
+    UPDATE reserves SET status = 'i' WHERE reserve_id = $1 RETURNING *;
+  `;
+  const res = await pool.query(query, [reserveId]);
+  return res.rows[0];
+};
